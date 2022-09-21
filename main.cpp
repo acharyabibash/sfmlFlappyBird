8e0af4e16f2fb4e7db0ec45c58f9fc6db03cd57e
acharyabibash <acharabibash@outlook.com>
2022-09-21 13:07:30 +0545

    SFML Game Engine Part4: Game Loop
    this concludes game engine part of SFML FLappy Bird series by Sonar Systems

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "Game.hpp"
 
 int main()
 {
+    Sonar::Game ( 800, 600, "SFML Starter");
     return EXIT_SUCCESS;
 	//random comments
 	//added openal32.dll files to project to avoid future pain
